8f6bc70c9f3e3dfc1014b992fc15c96bd7330e7f
RY2291 <prgr2498@gmail.com>
2024-08-31 11:19:51 +0900

    商品登録：バリデーションとフラッシュMsgの作成

--- resources/js/Pages/Items/index.vue
+++ resources/js/Pages/Items/index.vue
@@ -23,6 +23,9 @@ defineProps({
                         <!-- tailblocksからのコピー -->
                         <section class="text-gray-600 body-font">
                             <div class="container px-5 py-8 mx-auto">
+                                <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p-4">
+                                    {{ $page.props.flash.message }}
+                                </div>
                                 <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
                                     <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                                         商品登録

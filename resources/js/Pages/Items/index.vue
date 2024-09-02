4dc2d95497acdbf666dfcddb592ec7b3beb9552e
RY2291 <prgr2498@gmail.com>
2024-09-03 00:28:42 +0900

    商品削除の作成

--- resources/js/Pages/Items/index.vue
+++ resources/js/Pages/Items/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import FlashMessage from '@/Components/FlashMessage.vue';
 
 defineProps({
   items: Array
@@ -23,9 +24,7 @@ defineProps({
             <!-- tailblocksからのコピー -->
             <section class="text-gray-600 body-font">
               <div class="container px-5 py-8 mx-auto">
-                <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p-4">
-                  {{ $page.props.flash.message }}
-                </div>
+                <FlashMessage/>
                 <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
                   <Link as="button" :href="route('items.create')"
                     class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">

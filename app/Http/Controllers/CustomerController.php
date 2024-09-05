8bcb460ff9fdb1b168a37555d6f11d2ebdfc8647
RY2291 <prgr2498@gmail.com>
2024-09-05 23:36:57 +0900

    顧客登録機能作成

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -18,9 +18,9 @@ class CustomerController extends Controller
     public function index(Request $request)
     {
         $customers = Customer::searchCustomers($request->search)
-                        ->select('id', 'name', 'kana', 'tel')->paginate(20);
+            ->select('id', 'name', 'kana', 'tel')->paginate(20);
 
-        return Inertia::render('Customers/index', ['customers' =>$customers]);
+        return Inertia::render('Customers/index', ['customers' => $customers]);
     }
 
     /**
@@ -30,7 +30,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Customers/Create');
     }
 
     /**
@@ -41,7 +41,23 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        Customer::create([
+            'name' => $request->name,
+            'kana' => $request->kana,
+            'tel' => $request->tel,
+            'email' => $request->email,
+            'postcode' => $request->postcode,
+            'address' => $request->address,
+            'birthday' => $request->birthday,
+            'gender' => $request->gender,
+            'memo' => $request->memo,
+        ]);
+
+        return to_route('customers.index')
+        ->with([
+            'message' => '登録しました。',
+            'status' => 'success'
+        ]);
     }
 
     /**
